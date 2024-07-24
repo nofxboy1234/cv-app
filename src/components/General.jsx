3791aa51c4df3c1fc99418e18b636c48ca7ec314
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-24 11:57:47 +0200

    Put inputs inside form

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -6,46 +6,56 @@ export default function General() {
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
+  function submit(e) {
+    e.preventDefault();
+    console.log('submit!');
+  }
+
   return (
     <>
       <div>
         <h2>General</h2>
-        <div>
-          <label htmlFor="first-name">First Name </label>
-          <input
-            type="text"
-            id="first-name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="last-name">Last Name </label>
-          <input
-            type="text"
-            id="last-name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="phone-number">Phone number </label>
-          <input
-            type="text"
-            id="phone-number"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
+        <form onSubmit={submit}>
+          <div>
+            <label htmlFor="first-name">First Name </label>
+            <input
+              type="text"
+              id="first-name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="last-name">Last Name </label>
+            <input
+              type="text"
+              id="last-name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone-number">Phone number </label>
+            <input
+              type="text"
+              id="phone-number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     </>
   );
