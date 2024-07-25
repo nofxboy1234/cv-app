4abc456d7716d709c0de932d37f94a99651bebcb
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-25 14:59:41 +0200

    Add basic style to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import './styles/Component.css';
+
 import General from './components/general/General';
 import EducationIndex from './components/education/Index';
 import ExperienceIndex from './components/experience/Index';
